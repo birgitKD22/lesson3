ce9b368754dc15807249229405d257e3739ef3a9
birgitKD22 <114754287+birgitKD22@users.noreply.github.com>
2022-09-30 21:50:34 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            product: 'socks',
+            image: './assets/images/socks_green.jpg',
+            url: 'https://www.vuemastery.com/'
+        }
+    }
+})
